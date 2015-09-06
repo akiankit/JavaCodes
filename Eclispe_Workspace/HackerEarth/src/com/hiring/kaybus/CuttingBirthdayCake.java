00408469291473165bfecb13e4182f7d59055519
AnkitKumar <testankit20@gmail.com>
2015-09-06 20:00:56 +0530

    Committing seagate and kaybus hiring challenge on hackerearth answers
    For kaybus 2nd question we have not solved it yet correctly.

---
+++ Eclispe_Workspace/HackerEarth/src/com/hiring/kaybus/CuttingBirthdayCake.java
@@ -0,0 +1,28 @@
+package com.hiring.kaybus;
+
+import java.util.Scanner;
+
+public class CuttingBirthdayCake {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int tests = sc.nextInt();
+		for (int i = 0; i < tests; i++) {
+			double R = sc.nextInt();
+			double A = sc.nextInt();
+			double B = sc.nextInt();
+			double PIE = 3.14;
+			double circleArea = PIE * R * R;
+			double cutterArea = A * B;
+
+			if (circleArea < cutterArea) {
+				sb.append("EQUAL").append("\n");
+			} else {
+				sb.append("ALICE").append("\n");
+			}
+
+		}
+		System.out.println(sb.toString());
+		sc.close();
+	}
+}
---
+++ Eclispe_Workspace/HackerEarth/src/com/hiring/seagate/Agent007andSecretMission.java
@@ -0,0 +1,42 @@
+package com.hiring.seagate;
+
+import java.util.Scanner;
+
+public class Agent007andSecretMission {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tests = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<tests;i++){
+			String input = sc.next();
+			int ones = countOnesInString(input);
+			int n = sc.nextInt();
+			long sum = getSumUptoN(n);
+			if((sum&1) ==0){
+				sb.append(ones).append("\n");
+			}else{
+				sb.append(input.length()-ones).append("\n");
+			}
+		}
+		System.out.println(sb.toString());
+		sc.close();
+	}
+
+	private static long getSumUptoN(int n) {
+		if ((n & 1) == 0) {
+			return ((n >> 1) * (n + 1));
+		} else {
+			return ((n + 1) >> 1) * n;
+		}
+	}
+
+	private static int countOnesInString(String input) {
+		int count = 0;
+		for(int i=0;i<input.length();i++){
+			if(input.charAt(i)=='1')
+				count++;
+		}
+		return count;
+	}
+}
